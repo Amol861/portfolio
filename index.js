56e8fc27fa92ce1db1678e2b853ce258aa565891
Amol861 <59826798+Amol861@users.noreply.github.com>
2021-07-21 17:17:01 +0530

    Add PORTFOLIO files

---
+++ index.js
@@ -0,0 +1,25 @@
+$(".abcd").click(function()
+{
+  var audio = new Audio("office_ball_point_pen_click.mp3");
+  audio.play();
+});
+$(".firsto").click(function()
+{
+  var audio = new Audio("amol.mp3");
+  audio.play();
+});
+$(".secondo").click(function()
+{
+  var audio = new Audio("amol01.mp3");
+  audio.play();
+});
+$(".thirdo").click(function()
+{
+  var audio = new Audio("amol02.mp3");
+  audio.play();
+});
+$(".hii").click(function()
+{
+  var audio = new Audio("amol03.mp3");
+  audio.play();
+});
